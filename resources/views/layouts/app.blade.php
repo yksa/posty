f951cdc1d09a18e28eb5b5ccccd9629294d6f84c
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-06 21:28:02 +0630

    nav bar created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Document</title>
+    <title>Posty</title>
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Home</a></li>
+            <li><a href="" class="p-3">Dashboard</a></li>
+            <li><a href="" class="p-3">Post</a></li>
+        </ul>
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Ye Kyaw Swar Aung</a></li>
+            <li><a href="" class="p-3">Login</a></li>
+            <li><a href="" class="p-3">Register</a></li>
+            <li><a href="" class="p-3">Logout</a></li>
+        </ul>
+    </nav>
+
     @yield('content')
 </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    index
+    <div class="flex justify-center">
+        <div class="bg-white p-6 rounded-lg w-8/12">
+            Post
+        </div>
+    </div>
 @endsection
